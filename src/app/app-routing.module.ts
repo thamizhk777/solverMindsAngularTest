8c462076826cfc4d37cb8f7035202f9cdc63af68
thamizh <thamizhk777@gmail.com>
2021-07-04 22:58:33 +0530

    update for sidemenu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { BrandsComponent } from './components/brands/brands.component';
+import { BrandsModule } from './components/brands/brands.module';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductsModule } from './components/products/products.module';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: AppComponent
+  },
+  {
+    path: 'products',
+    component: ProductsComponent
+    // loadChildren: () => import('./components/products/products.module').then(m => m.ProductsModule)
+  },
+  {
+    path: 'brands',
+    component: BrandsComponent
+    // loadChildren: () => import('./components/brands/brands.module').then(m => m.BrandsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'solverMindsAngularTest';
+
+  mode = 'side'
+  opened = true;
+  layoutGap = '64';
+  fixedInViewport = true;
+  title = 'SolverMinds Angular Test';
+  curentTab = 'products';
+  
+  routes =  [
+    { path: 'products', label: 'Products' },
+    { path: 'brands', label: 'Brands' }
+   ];
+
+  constructor(private router: Router){
+  
+  }
+
+  changeTab(tab){
+    this.curentTab  = tab;
+    this.router.navigate([tab]);
+  }
+
 }
